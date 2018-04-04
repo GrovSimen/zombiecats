07e3361ccb0917a0aa10cbfdd447f74a344af2ed
DESKTOP-68OBJRN\jorge <jorgenaw@hotmail.com>
2018-04-04 13:37:45 +0200

    Added map 1 and map 2 and zoom function

--- New Unity Project/Assets/Scripts/ProjectileController.cs
+++ New Unity Project/Assets/Scripts/ProjectileController.cs
@@ -22,11 +22,11 @@ public class ProjectileController : MonoBehaviour {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0)
+        if (lifeTime <= 0)
         {
             Destroy(gameObject);
         }
-	}
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -35,6 +35,16 @@ public class ProjectileController : MonoBehaviour {
             other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
             Destroy(gameObject);
         }
+        else if(other.gameObject.tag == "Object")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
     }
+
+
 }
