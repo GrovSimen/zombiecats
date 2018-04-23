bf4c41a674889f9fe2c43eace5bd497457a5819c
Unknown <theo_u@hotmail.com>
2018-04-23 19:38:21 +0200

    Added death menu

--- New Unity Project/Assets/Scripts/DemoMenu.cs
+++ New Unity Project/Assets/Scripts/DemoMenu.cs
@@ -15,5 +15,9 @@ public class DemoMenu : MonoBehaviour {
         Debug.Log("Quit!");
         Application.Quit();
     }
+
+	public void QuitToMenu(){
+		SceneManager.LoadScene ("DemoMenu");
+	}
 		
 }
